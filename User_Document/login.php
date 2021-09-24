45d815e89d1857c11b9e92a864b2f5f6ecab8cb5
bewebdeveloper <91284482+bewebdeveloper@users.noreply.github.com>
2021-09-24 11:32:23 +0530

    Update login.php

--- User_Document/login.php
+++ User_Document/login.php
@@ -3,12 +3,11 @@ $servername="localhost";
 $name="root";
 $password="";
 $dbname="Student";
-mysqli_connect
-// $a=$_GET['Email'];
-// $b=$_GET['password'];
-// $conn=mysqli_connect($servername,$name,$password,$dbname);
-// $task="insert into mylog(Email,password)VALUES('$a','$b')";
+$a=$_GET['Email'];
+$b=$_GET['password'];
+$conn=mysqli_connect($servername,$name,$password,$dbname);
+$task="insert into mylog(Email,password)VALUES('$a','$b')";
 
-// $result=mysqli_query($conn,$task);
-//         mysqli_close($conn);
+$result=mysqli_query($conn,$task);
+        mysqli_close($conn);
 ?>
