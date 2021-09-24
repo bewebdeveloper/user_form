d2599e37cbc1aa9315dda4d29546a18895752030
bewebdeveloper <91284482+bewebdeveloper@users.noreply.github.com>
2021-09-24 11:30:15 +0530

    Add files via upload

---
+++ User_Document/login.php
@@ -0,0 +1,14 @@
+<?php
+$servername="localhost";
+$name="root";
+$password="";
+$dbname="Student";
+mysqli_connect
+// $a=$_GET['Email'];
+// $b=$_GET['password'];
+// $conn=mysqli_connect($servername,$name,$password,$dbname);
+// $task="insert into mylog(Email,password)VALUES('$a','$b')";
+
+// $result=mysqli_query($conn,$task);
+//         mysqli_close($conn);
+?>
---
+++ User_Document/registration.php
@@ -0,0 +1,18 @@
+<?php
+$servername="localhost";
+$name="root";
+$password="";
+$dbname="user";
+$a=$_GET['name'];
+$b=$_GET['address'];
+$c=$_GET['id'];
+$d=$_GET['mob'];
+$conn=mysqli_connect($servername,$name,$password,$dbname);
+$task="insert into form(name,address,id,mob)VALUES('$a','$b','$c',$d)";
+
+$result=mysqli_query($conn,$task);
+        mysqli_close($conn);
+?>
+<?php
+    header("location:login.html");
+?>
